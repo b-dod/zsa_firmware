1b5db951039394dc28a047c389b69807a9c33f89
Bryson Dodwell <thecreaturesound@gmail.com>
2024-02-27 09:23:41 -0800

    start adding layout config calls in my user .h file

--- users/b-dod/b-dod.h
+++ users/b-dod/b-dod.h
@@ -1,1 +1,10 @@
 #pragma once
+
+//
+// which HD alpha variation are we using?
+//
+// defines all variation dependent constants/files/keycodes, etc.
+// they will be used in the respective keymap for each keyboard
+
+// #include "handsdown/vb-config.h" // definitions for the Alpha layer and mnemonic combos
+// #include "moutis_layers.h" // definitions for all the other layers
