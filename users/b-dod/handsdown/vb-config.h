ada728a03a5b4fd9e8493a544215a039a024756d
Bryson Dodwell <thecreaturesound@gmail.com>
2024-05-01 14:39:28 -0700

    Update paste combo to two fingers now that I'm on Vibranium

--- users/b-dod/handsdown/vb-config.h
+++ users/b-dod/handsdown/vb-config.h
@@ -169,7 +169,7 @@
 #define HD_undo_keys  HD_LB4, HD_LB3   // undo
 #define HD_redo_keys  HD_LB4, HD_LB3, HD_LB2   // redo
 #define HD_copy_keys  HD_LB3, HD_LB2   // copy (hold for cut)
-#define HD_pste_keys  HD_LB3, HD_LB2, HD_LB1   // paste (hold for paste-match)
+#define HD_pste_keys  HD_LB2, HD_LB1   // paste (hold for paste-match)
 #define HD_esc_keys   HD_LM1, HD_LM4   // esc
 
 #define HD_tab_keys  HD_LT4, HD_LT3     // tab
