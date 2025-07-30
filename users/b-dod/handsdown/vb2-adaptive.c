1d9669223b2a81dc20421b9421e884dc994b1a7c
Bryson Dodwell <thecreaturesound@gmail.com>
2025-07-29 18:35:55 -0700

    add DJ adaptive on "GJ"

--- users/b-dod/handsdown/vb2-adaptive.c
+++ users/b-dod/handsdown/vb2-adaptive.c
@@ -187,6 +187,10 @@ bool process_adaptive_key(uint16_t keycode, const keyrecord_t *record) {
                 tap_code(KC_L);
                 return_state = false; // done.
                 break;
+            case KC_G:
+                tap_code(KC_BSPC);
+                tap_code(KC_D);
+                break; // and let current keycode send normally
             }
             break;
 
